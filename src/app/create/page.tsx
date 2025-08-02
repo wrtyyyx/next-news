ba57c8cba42144727bc3d9e15b60801746cf45b8
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2025-08-02 14:17:49 +0300

    change middleware

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,18 +1,11 @@
 import CreateView from '@/sections/create/create-view';
 import React from 'react';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-import  authOptions  from '@/config/auth';
+
 export  const metadata = {
   title: 'Create News',
   description: 'Create a new news article',
 };
 const page = async () => {
-  const session = await getServerSession(authOptions)
-
-   if (!session?.user || session.user.role !== 'admin') {
-    return redirect('/') 
-  }
   return (
     <CreateView />
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ import { match } from 'assert';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/profile/:path*','/create/:path*']
+  matcher: ['/dashboard/:path*', '/profile/:path*','/create', '/edit/:path*',],
 }
